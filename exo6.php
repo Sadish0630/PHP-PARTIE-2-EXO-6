7a6102447100c98b48decf8aea612262383a96b2
audemar <audemarsadish@gmail.com>
2021-12-08 11:10:31 +0100

    exo6 terminé

---
+++ exo6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css" />
+    <title>PARTIE 2 EXO 6</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $age = 40;
+            if ($age >= 18) {
+                echo 'Tu es majeur'; }
+            else {
+                echo 'Tu n\'es pas majeur';}
+        ?>
+    </p>
+
+</body>
+
+</html>
